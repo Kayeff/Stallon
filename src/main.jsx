b583344f592204d7667156c9a9ee46070d698d00
Kaif <kaifsaiyed896@gmail.com>
2025-02-13 15:32:02 +0530

    updated minor github changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 import App from "./App.jsx";
@@ -11,7 +11,7 @@ import Branches from "./pages/Branches.jsx";
 import Blog from "./pages/Blog.jsx";
 import Pricing from "./pages/Pricing.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />, // App is the layout
