b4d46ba56d4593f6742906e180dc124a4661e8b7
Kaif <kaifsaiyed896@gmail.com>
2025-03-06 21:48:33 +0530

    modified lenis for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
         easing: (t) => 1 - Math.pow(1 - t, 2),
         syncTouch: true,
         smoothTouch: true,
+        touchMultiplier: 1.5,
       }}
     >
       <main className="w-full min-h-screen bg-black relative flex items-center justify-between flex-col">
