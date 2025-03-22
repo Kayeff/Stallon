85317fe17cb5f215c6da05a04ab68c3304ce732d
Kaif <kaifsaiyed896@gmail.com>
2025-03-22 19:26:29 +0530

    make changes for performance-3

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,13 @@ export default defineConfig({
     compression({ algorithm: "brotliCompress", ext: ".br" }), // Brotli compression
     compression({ algorithm: "gzip", ext: ".gz" }), // Gzip compression
   ],
+  optimizeDeps: {
+    include: ["framer-motion", "lenis"], // Ensure these are properly bundled
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true, // Fixes issues with ESM modules
+    },
+  },
   base: "/Stallon/",
 });
